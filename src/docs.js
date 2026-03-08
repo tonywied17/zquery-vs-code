59b06a2a6d8ad987a1db9c7f3dbaee2d2a714cc5
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2026-03-08 00:41:37 -0500

    0.4.0

--- src/docs.js
+++ src/docs.js
@@ -479,7 +479,7 @@ const dollarMethods = [
     name: 'version',
     kind: 'Property',
     detail: 'string',
-    documentation: "Library version string (e.g. `'0.5.1'`).",
+    documentation: "Library version string (e.g. `'0.6.1'`).",
     insertText: 'version',
   },
   {
